25088f1df34ed9bedbfaffbe5dbb58fd5f662084
Victhorin <victor.rib66@gmail.com>
2022-02-24 20:37:38 -0300

    primeiro commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        gato gt = new gato();
+        System.out.println(gt);
+
+       /* int a = 5;
+        int b = 6;
+
+        System.out.println("Hello World " + (a + b));
+*/
+    }
+}
