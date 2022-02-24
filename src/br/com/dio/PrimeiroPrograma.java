bc0891297147323fe8e3702c112b80605a77b7fa
Victhorin <victor.rib66@gmail.com>
2022-02-24 20:48:03 -0300

    adcionando a classe livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,10 +7,54 @@ public class PrimeiroPrograma {
         gato gt = new gato();
         System.out.println(gt);
 
+        livro lv = new livro();
+        System.out.println(lv);
+
        /* int a = 5;
         int b = 6;
 
         System.out.println("Hello World " + (a + b));
 */
+
+
+    } static class livro {
+        private String nome;
+        private int npag;
+
+        public livro(String nome, int npag) {
+            this.nome = nome;
+            this.npag = npag;
+        }
+
+        public livro() {
+
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public int getNpag() {
+            return npag;
+        }
+
+        public void setNpag(int npag) {
+            this.npag = npag;
+        }
+
+        @Override
+        public String toString() {
+            return "livro{" +
+                    "nome='" + nome + '\'' +
+                    ", npag=" + npag +
+                    '}';
+        }
     }
+
+
+
 }
